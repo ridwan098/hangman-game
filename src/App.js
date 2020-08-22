fc654c7a3302e36d1be686e3f6685b91accf6d09
Ridwan <ridwanishola97@gmail.com>
2020-08-22 16:18:49 +0100

    components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Header from './components/header/header';
+import Figure from './components/figure/figure';
 class App extends Component {
 
 
@@ -8,7 +9,8 @@ class App extends Component {
     render() {
         return (
             <div>
-                Hello worldies! no
+                <Header />
+
             </div>
         )
     }
---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Hangman</h1>
+            <p>Enter a letter to find the hidden word!</p>
+        </div>
+    )
+}
+
+
+export default Header;

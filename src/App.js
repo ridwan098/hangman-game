1077e12c63eb0e63f1bfc126b547ca6432c4149e
Ridwan <ridwanishola97@gmail.com>
2020-08-22 17:27:31 +0100

    visual changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/header/header';
 import Figure from './components/figure/figure';
-class App extends Component {
+import WrongLetters from './components/wrongLetters/wrongLetters';
+import Word from './components/word';
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 
+const App = () => {
+    const [playable, setPlayable] = useState(true);
+    const [correctLetters, setCorrectLetters] = useState([]);
+    const [wrongLetters, setWrongLetters] = useState([]);
 
-    render() {
-        return (
-            <div>
-                <Header />
+    useEffect(() => {
+        const handleKeydown = event => {
+            const { key, keyCode } = event;
 
+
+            if (playable && keyCode >= 65 && keyCode <= 90) {
+                const letter = key.toLowerCase();
+
+                if (selectedWord.includes(letter)) {
+                    if (!correctLetters.includes(letter)) {
+                        setCorrectLetters(currentLetters => [...currentLetters, letter])
+
+                    } else {
+                        // showNotification();
+                    }
+                } else {
+                    if (!wrongLetters.includes(letter)) {
+                        setCorrectLetters(wrongLetters => [...wrongLetters, letter])
+
+                    } else {
+                        // showNotification();
+                    }
+                }
+            }
+
+        }
+        window.addEventListener('keydown', handleKeydown)
+
+        return () => window.removeEventListener('keydown', handleKeydown);
+    }, [correctLetters, wrongLetters, playable]);
+
+    return (
+        <div>
+            <Header />
+            <div className='game-container'>
+                <Figure />
+                <WrongLetters wrongLetters={wrongLetters} />
+                <Word selectedWord={selectedWord} correctLetters={correctLetters} />
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
 

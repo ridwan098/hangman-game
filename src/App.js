e25302dd6114450db0021d91b6080140ed2d2fa7
Ridwan <ridwanishola97@gmail.com>
2020-08-22 16:05:32 +0100

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                Hello worldies!
+                Hello worldies! no
             </div>
         )
     }
